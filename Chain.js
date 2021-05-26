fde3b11a81eeeadfb0f1f55b21bf66f52b358f6b
Lightshift07 <82979690+Lightshift07@users.noreply.github.com>
2021-05-26 18:45:00 -0400

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,19 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var option = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            stiffness: 0.04,
+            lenght: 20
+        }
+      this.chain = Constraint.create(option) ;
+      World.add(world,this.chain)
+        
+    }
+    display(){
+        var posA = this.chain.bodyA.position
+        var posB = this.chain.bodyB.position
+        strokeWeight(4);
+        line(posA.x,posA.y,posB.x,posB.y)
+    }
+}
